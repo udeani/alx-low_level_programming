7fd0a17d62f175b7f87414ba3ef03105cdd99f6d
udeani <udeani.henry@gmail.com>
2022-06-02 14:01:40 -0700

    adding extra script 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+/*
+ * File: 101-quote.c
+ * Auth: github.com/udeani
+ *
+ *
+ */
+
+#include <unistd.h>
+
+/**
+ * main - outputs "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *        followed by a new line, to standard error.
+ *
+ * Return: 1.
+ *
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
