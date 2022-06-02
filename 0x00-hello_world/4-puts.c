aff362341b80977e8a8ce8817996b79be7db5ff1
udeani <udeani.henry@gmail.com>
2022-06-02 13:02:28 -0700

    adding more commentsto the C 4-puts code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
+/*
+ * File: 4-puts.c
+ */
+
 #include <stdio.h>
 /**
- *  comment line
+ * main - Prints "Programming is like a multilingual puzzle
+ * followed by a new line
+ *
+ * Return: ruturns 0.
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
