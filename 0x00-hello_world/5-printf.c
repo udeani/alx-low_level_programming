bad9b57715c2bac21ab8e58fc1abb5d5aabb3406
udeani <udeani.henry@gmail.com>
2022-06-02 13:12:25 -0700

    adding script to print a defined string with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: github.com/udeani
+ */
+
+#include <stdio.h>
+
+/*
+ * main: prints with proper grammer,
+ * but the outcome is a piece of art
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper gramma, but the outcome is a piece of art");
+	return (0);
+}
