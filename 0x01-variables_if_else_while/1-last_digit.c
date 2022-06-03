9731515bb81f1896b6e59b8a0e1143804450b3ac
udeani <udeani.henry@gmail.com>
2022-06-03 11:51:03 -0700

    adding script 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+/*
+ *File: 0-positive_or_negative.c
+ *Auth: github.com/udeani
+ */
+
+
+#include <stdlib.h>
+#include <time.h>
+
+/**main - Prints a random number and checks if the number is positive,
+ *     negetive,
+ *     or zero.
+ *
+ * Return: 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is the second day of C low level programming
