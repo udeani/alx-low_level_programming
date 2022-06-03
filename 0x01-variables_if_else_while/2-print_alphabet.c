988e83760ca16b016a3ae73fbc0ba131420a6bc9
udeani <udeani.henry@gmail.com>
2022-06-03 13:23:11 -0700

    adding script 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-print_alohabet.c
+ * Auth: github.com/udeani
+ */
+#include <stdio.h>
+/**
+ * main - Prints the alphabets in small letters
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	return (0);
+}
