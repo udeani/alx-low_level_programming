796d0451efac1ed26359b0f9054fab87071770ef
udeani <udeani.henry@gmail.com>
2022-06-03 11:58:51 -0700

    fixing code error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -25,11 +25,11 @@ int main(void)
 	
 	if (n > 0)
 		printf("%d is positive\n", n);
-	if (n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-
+i
 
 	return (0);
 }
