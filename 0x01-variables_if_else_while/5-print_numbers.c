1c6527c2dec76069ace143c7d8721dddf28a016e
udeani <udeani.henry@gmail.com>
2022-06-03 13:52:24 -0700

    adding script 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: github.com/udeani
+ */
+#include <stdio.h>
+/**
+ * main - code to print numbers in base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 0; n <= 10; n++)
+		printf("%d",n);
+	printf("\n");
+	return (0);
+}
