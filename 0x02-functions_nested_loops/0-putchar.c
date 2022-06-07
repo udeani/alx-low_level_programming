44175eab8cb47969013b5cdb3c15c534520228e7
udeani <udeani.henry@gmail.com>
2022-06-07 11:57:59 -0700

    adding script 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-putchar.c
+ * Auth: udeani
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Script to print _putchar followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf('-putchar');
+	printf("\n");
+	return (0);
+}
