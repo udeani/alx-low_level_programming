29c35ce435629482c86a8c8c90391c43a5f7c774
udeani <udeani.henry@gmail.com>
2022-06-08 14:46:14 -0700

    adding script 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/*
+ * File: 10-add.c
+ * Auth; udeani henry
+ */
+
+#include "main.h"
+
+/**
+ * add - adds two integers together
+ * @x: first input
+ * @y: second input
+ * Return: sum of the inputs
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
