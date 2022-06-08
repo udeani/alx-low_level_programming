485e9bfdc8b743099eec2d442608bc281eeb67b1
udeani <udeani.henry@gmail.com>
2022-06-08 13:34:12 -0700

    adding script 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,38 @@
+/*
+ * File: 5-sign.c
+ * Auth: udeani henry
+ */
+
+#include "main.h"
+
+/**
+ * print_sign - prints the sign value of a number
+ * @n: input from test file
+ * Return: either 1, 0, 0r -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+}
+	
+
