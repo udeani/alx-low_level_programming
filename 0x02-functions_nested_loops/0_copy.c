62b3c2c5779b2120483afbd9fc9fc2f517228177
udeani <udeani.henry@gmail.com>
2022-06-08 11:33:51 -0700

    fixing errors

--- 0x02-functions_nested_loops/0_copy.c
+++ 0x02-functions_nested_loops/0_copy.c
@@ -13,11 +13,11 @@ int main(void)
 {
 	char c[] = "_putchar";
 	int i;
-
-	for (i = 0; i < 8; i++)
+	for (i=0, i < 8, i++))
 	{
 		_putchar(c[i]);
 	}
+
 	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
  * Desc: header file containing declarations for all
  * the functions
  */
-int _putchar(char c);
+int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
