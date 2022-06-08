c478ac908dbcc3faa55b73557366f147d72b4d67
udeani <udeani.henry@gmail.com>
2022-06-08 12:12:36 -0700

    adding script 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * File: 1-alphabet.c
+ * Auth: Udeani henry
+ */
+/** print_alphabet - prints alphabet in lower case follwed by a new line
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a', letter <= 'z', letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
+
