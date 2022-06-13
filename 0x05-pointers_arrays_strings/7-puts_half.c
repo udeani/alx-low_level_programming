ed9eab2ed8c030751c84ea4228f7c5a903492a63
udeani <udeani.henry@gmail.com>
2022-06-13 12:41:23 -0700

    adding script 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+/*
+ * File: 7-puts_half.c
+ * Auth: udeani henry
+ */
+
+#include "main.h"
+
+/**
+ * puts_half - only prints the second half of a string and a new line
+ * @str: string to half
+ */
+
+void puts_half(char *str)
+{
+	int index = 0, len = 0, n;
+
+	while (str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
