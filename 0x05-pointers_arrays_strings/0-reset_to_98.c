a145710c33190687195fa25b460e581957bbed9c
udeani <udeani.henry@gmail.com>
2022-06-13 05:31:37 -0700

    adding README.md fle and first script

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: udeani henry
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reaet_to_98 - takes a pointer to an int as a parameter
+ *     and updates the value it points to 98.
+ * Return: nothing
+ */
+
+void reser_to_98(int *n)
+{
+	*n = 98;
+}
