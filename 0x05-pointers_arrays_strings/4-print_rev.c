38837f1d728140b8603cf38aadc1c00ed4c96be4
udeani <udeani.henry@gmail.com>
2022-06-13 11:22:59 -0700

    adding script 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+/*
+ * File: 4-print_rev.c
+ * Auth: udeani henry
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - prints any passed string in reverse.
+ * @s: the string to reverse
+ * Return: will always return reversed string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i--]);
+	}
+	_putchar('\n');
+}
