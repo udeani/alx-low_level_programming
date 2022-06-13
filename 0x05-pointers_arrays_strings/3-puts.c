59dc3a1639cbe45d9f1bb6dbdffdee73347f74dc
udeani <udeani.henry@gmail.com>
2022-06-13 06:24:57 -0700

    fixing errors

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,6 +5,12 @@
 
 #include "main.h"
 
+/**
+ * _puts - prints the passed string and new line.
+ * @str: the passed string
+ * Return: lenght of the string.
+ */
+
 void _puts(char *str)
 {
 	while (*str)
