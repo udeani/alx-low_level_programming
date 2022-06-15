4d7355bc118770bd0663f90f5ee6877fc5bc5d91
udeani <udeani.henry@gmail.com>
2022-06-15 14:28:21 -0700

    adding script 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+/*
+ * File: 6-cap_string.c
+ * Auth: udeani henry
+ */
+
+#include "main.h"
+
+/**
+ * cap_string - function to capitalize first letters of a
+ *     word in a string.
+ * @str: the string to capitalize
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int n = 0;
+
+	char *temp = " \t\n,;.!?\"(){}";
+
+	while (str[n])
+	{
+		while (!(str[n] >= 'a' && str[n] <= 'z'))
+		{
+			for (str[n] >= temp[0] && str[n] <= temp[13])
+				str[n + 1] _= 32;
+			n++;
+		}
+		n++;
+	}
+	return (str);
+}
