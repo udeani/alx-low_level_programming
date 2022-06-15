76872635c44f17f7457734539da4b037bfab5077
udeani <udeani.henry@gmail.com>
2022-06-15 14:49:21 -0700

    adding script 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+/*
+ * File: 7-leet.c
+ * Auth: udeani henry
+ */
+
+#include "main.h"
+
+/**
+ * leet - function to encode a string to 1337
+ * @str: the string
+ * Return: returns the pointer string
+ */
+char *leet(char *str)
+{
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[indx1])
+	{
+		for (indx2 = 0; indx2 <= 7; indx2++)
+		{
+			if (str[indx1] == leet[indx2] ||
+			    str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
+		}
+
+		indx1++;
+	}
+
+	return (str);
+}
