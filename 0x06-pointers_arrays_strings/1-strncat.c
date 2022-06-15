5a8bd46334508d49c5a48b6ba89f1c0c20b5662e
udeani <udeani.henry@gmail.com>
2022-06-15 12:35:22 -0700

    fixing errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,13 +14,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int index = 0, dest_len = 0;
 
-	for(i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
