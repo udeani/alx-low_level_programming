5b73e5b771c778d9f7a542fa4d9cb09650dddf38
udeani <udeani.henry@gmail.com>
2022-06-15 12:10:51 -0700

    fixing erros

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,20 +7,27 @@
 
 /**
  * *_strcat - function that concats or joins two strings.
- * @*dest: string to append to
+ * @dest: string to append to
  * @src: string to join to dest
  * Return: returnsa pointer of string
  */
 
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	i = 0;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+		j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	/*dest[i] = ('\0')*/
 
 	return (dest);
 }
