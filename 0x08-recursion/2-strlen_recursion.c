feaf5fc7239d502ec68f8d759b8ecfc927fdd256
udeani <udeani.henry@gmail.com>
2022-06-22 12:15:56 -0700

    adding task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Auth: udeani henry
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to retuurn the lenght of a string
+ * @s: the string buffer to return
+ * Return: the length o f the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s+1);
+	}
+	return (len);
+}
