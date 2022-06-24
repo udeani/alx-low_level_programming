0628ed10ea341a3359a4c1ca30c4dc6d233036c0
udeani <udeani.henry@gmail.com>
2022-06-24 13:57:41 -0700

    addin extra task

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+	while(*s != '\0')
+		s++;
+	return (0);
+}
