95afbe31e96af6c8e6c83ef2af6c61366dd7cec5
udeani <udeani.henry@gmail.com>
2022-06-27 12:13:50 -0700

    adding script 0 and readme.md file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+/*
+ * File: 0-create_array.c
+ * Auth: udeani henry
+ */
+
+#include "main.h"
+
+/**
+ * create_array - function to create an array of chars.
+ * @size: the size of the char to initialize
+ * @c: the char to initialize.
+ * Return: char buffer initialized.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+		return(NULL);
+
+	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
+}
