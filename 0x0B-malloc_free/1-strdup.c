b5e982317eb4215b0c904fb7ef0c555b242f36a3
udeani <udeani.henry@gmail.com>
2022-06-27 12:27:45 -0700

    adding task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+/*
+ * File: 1-strdup.c
+ * Auth: udeani henry
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ *@str: string being allocated in memory
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	int i, str_len;
+	char *dupe;
+
+	if (str == NULL)
+		return (NULL);
+	for (str_len = 0; str[str_len] != '\0'; str_len++)
+		;
+	dupe = malloc(sizeof(char) * str_len + 1);
+	if (dupe == NULL)
+		return (NULL);
+	for (i = 0; i < str_len; i++)
+		dupe[i] = str[i];
+	return (dupe);
+}
