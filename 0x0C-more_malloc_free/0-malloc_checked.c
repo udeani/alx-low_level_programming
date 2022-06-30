a3691892f06b47bad4a6babffb9091405ba819a8
udeani <udeani.henry@gmail.com>
2022-06-30 11:37:49 -0700

    fixing errors

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,6 +9,7 @@
 /**
  * malloc_checked - function to allocate memory using malloc.
  * @b: positive value to allocate.
+ * Return: a pointer to the memory allocated.
  */
 
 void *malloc_checked(unsigned int b)
