910be04b41c6f23bac1abc53ed6fbdf711098967
udeani <udeani.henry@gmail.com>
2022-07-04 13:10:03 -0700

    adding all the task and a readme

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Auth: Udeani henry
+ * Desc: Header file that defines a function like macro SUM(x, y)
+ *       that computes the sum value of a number x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
