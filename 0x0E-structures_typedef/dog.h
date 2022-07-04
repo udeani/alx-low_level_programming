289c65e3e5e25859e7cec4bcc5cbc6da5cd1fb13
udeani <udeani.henry@gmail.com>
2022-07-04 13:34:45 -0700

    adding readme and task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Auth: Udeani henry
+ * Desc: Header file to define a new type strct dog.
+ */
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
