78826623822cd6bcd80a698f5c7e37fbdd1d69ea
udeani <udeani.henry@gmail.com>
2022-07-14 13:24:34 -0700

    adding readme and task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+/*
+ * File: 0-print_list.c
+ * Auth: Udeani henry
+ */
+
+#include "lists.h"
+
+/**
+ * print_list - function to print the list items and there size.
+ * @h: the pointer to the listed list
+ * Return: the number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This folder contains file for the linked list task and the header file
