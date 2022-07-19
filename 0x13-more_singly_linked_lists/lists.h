47487075d079a000c514379e7a93d7012ec6dfff
udeani <udeani.henry@gmail.com>
2022-07-19 14:17:02 -0700

    fixing errors

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,8 +3,9 @@
 
 /*
  * File: lists.h
- * Auth: Udeani henry
- * Desc: Header file with prototypes for more linked list assignment
+ * Auth: Udeani Henry
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
  */
 
 #include <stdlib.h>
@@ -14,14 +15,14 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure 
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
-	    int n;
-	    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -38,3 +39,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
+#endif /* LISTS_H */
